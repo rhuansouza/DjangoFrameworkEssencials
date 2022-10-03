10e4d0ac5ea1dd9cce1244c9017d44f3c34cf520
Rhuan Costanzo <rhuan@cooxupe.com>
2022-10-03 18:12:13 -0300

    feat: django1

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    context = {'curso': 'Programacao web com Django'}
+    return render(request, 'index.html', context)
+
+
+def contato(request):
+    return render(request, 'contato.html')
--- django1/settings.py
+++ django1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r1yr*@9l_xb+##mq!!e*$#nqq6g1$5+uc*&3j&&%o7cu1htu7o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
